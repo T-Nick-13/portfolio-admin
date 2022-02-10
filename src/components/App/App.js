8e54101ab8f23774a7b41ab698cb74d7c53419ea
Nikita Titov <netitov@gmail.com>
2022-02-10 10:31:24 +0300

    подключение шрифтов, normalize.css

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+//import Header from '../Header/Header';
+
+function App() {
+
+  return (
+    <div className="page">
+      <div className="page__wrapper">
+
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
