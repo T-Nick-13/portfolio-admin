f9e64ef1f9a766f160b250dba90a1edb08468db8
Nikita Titov <netitov@gmail.com>
2022-02-26 10:58:20 +0300

    добавлены иконки комп Navigation; адаптивность для комп Navigation и Main

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
+import uploadLogo from '../../images/light/upload_icon_149852.svg';
+import deletedLogo from '../../images/light/deleteCircle.png';
+import statisticLogo from '../../images/light/bar_chart_bar_graph_statistics_diagram_icon_187100.svg';
 
 function Navigation(props) {
 
@@ -16,14 +19,18 @@ function Navigation(props) {
     <nav className="menu">
       <div className="menu__container">
       <h1 className="menu__heading" onClick={handleLogoClick}>Natalya Stafeeva</h1>
+      <div className="menu__heading_ts" onClick={handleLogoClick}><p>NS</p></div>
       <ul className="menu__ul">
         <li>
+          <img className="menu__icon" src={uploadLogo} alt="download-icon" onClick={handleClickLoading}></img>
           <Link to="#" className="menu__li" onClick={handleClickLoading}>Загрузить</Link>
         </li>
         <li>
+          <img className="menu__icon" src={deletedLogo} alt="delete-icon"></img>
           <Link to="#" className="menu__li">Удалить</Link>
         </li>
         <li>
+          <img className="menu__icon" src={statisticLogo} alt="stat-icon"></img>
           <Link to="#" className="menu__li">Статистика</Link>
         </li>
       </ul>
